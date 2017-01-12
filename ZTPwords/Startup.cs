3fb12fa85f099949534430a89ba6d422448cd066
Szymon Bakunowicz <szymon.bakunowicz@gmail.com>
2017-01-12 20:41:49 +0100

    init

---
+++ ZTPwords/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZTPwords.Startup))]
+namespace ZTPwords
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
