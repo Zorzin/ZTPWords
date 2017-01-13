710dd022f20fa5a825a67432d3f47f8aea179885
Vaviorky <danieloss15@gmail.com>
2017-01-13 21:20:25 +0100

    StrategyController and Strategy Interface with implemetation classes

---
+++ ZTPwords/Logic/Strategy/LearningMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZTPwords.Logic.Strategy
+{
+    public class LearningMode :IMode
+    {
+        public void AnswerQuestion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
