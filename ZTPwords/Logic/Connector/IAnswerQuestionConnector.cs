5097685aa216220dc57788c35b3c73a540ac226f
Szymon Bakunowicz <szymon.bakunowicz@gmail.com>
2017-01-17 12:14:46 +0100

    Final

--- ZTPwords/Logic/Connector/IAnswerQuestionConnector.cs
+++ ZTPwords/Logic/Connector/IAnswerQuestionConnector.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ZTPwords.Models.QuestionViewModels;
 
 namespace ZTPwords.Logic.Connector
 {
     interface IAnswerQuestionConnector
     {
+        QuestionModel GetQuestion();
+
     }
 }
